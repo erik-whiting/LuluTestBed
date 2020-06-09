73a8c728d8cf8c10d27f2a5e066679fc4db05861
erik-whiting <erik@erikwhiting.com>
2020-06-08 23:26:05 -0500

    Finish bands page

---
+++ MusicStore/js/bands.js
@@ -0,0 +1,17 @@
+const getBands = async () => {
+    const response = await fetch('http://localhost:5000/api/v1/resources/bands');
+    const returnJson = await response.json();
+    return returnJson;
+};
+
+let allBands = getBands();
+allBands.then(function(results) {
+    let band_table = document.getElementById('band-table');
+
+    results.forEach((band) => {
+        let row = band_table.insertRow(band.id);
+        let cell = row.insertCell(0);
+        row.setAttribute('id', `band-${band.id}`);
+        cell.innerHTML = band.bandname;
+    });
+});
