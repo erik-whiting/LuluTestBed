69d72283cd8c29317e213fe6b9c27a95f1480ada
erik-whiting <erik@erikwhiting.com>
2020-07-12 23:35:26 -0500

    Update randomizer in sales.sql

--- seed_data/sales.sql
+++ seed_data/sales.sql
@@ -3,10 +3,10 @@ BEGIN
   FOR counter IN 1..100 LOOP
     INSERT INTO Sale (Id, DateOfSale) VALUES (counter, RandomDate());
     INSERT INTO LineItem (SaleId, AlbumId, Price) VALUES 
-      (counter, (RANDOM()*(66-2)+2)::INT, 20*random()::decimal(12,2)::MONEY);
+      (counter, (RANDOM()*(65-2)+2)::INT, 20*random()::decimal(12,2)::MONEY);
     INSERT INTO LineItem (SaleId, AlbumId, Price) VALUES 
-      (counter, (RANDOM()*(66-2)+2)::INT, 20*random()::decimal(12,2)::MONEY);
+      (counter, (RANDOM()*(65-2)+2)::INT, 20*random()::decimal(12,2)::MONEY);
     INSERT INTO LineItem (SaleId, AlbumId, Price) VALUES 
-      (counter, (RANDOM()*(66-2)+2)::INT, 20*random()::decimal(12,2)::MONEY);
+      (counter, (RANDOM()*(65-2)+2)::INT, 20*random()::decimal(12,2)::MONEY);
   END LOOP;
 END; $$
