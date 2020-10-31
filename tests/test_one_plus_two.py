0f9f7b90d1c7f046c3eb46b514d28976843a2c06
erik-whiting <erik@erikwhiting.com>
2020-10-30 19:48:34 -0500

    Add test directory and test

---
+++ tests/test_one_plus_two.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestOnePlusTwo(unittest.TestCase):
+    def test_one_plus_two(self):
+        x = 1
+        y = 2
+        z = x + y
+        self.assertEqual(z, 3)
+
+if __name__ == '__main__':
+    unittest.main()
